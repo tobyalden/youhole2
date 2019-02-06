40c68ac1428479bf47ad0d14b1d5c494e54fef53
Toby Alden <toby.alden@gmail.com>
2019-02-05 23:03:44 -0800

    Change starting video to EMBASSY album

--- js/scripts.js
+++ js/scripts.js
@@ -59,7 +59,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '390',
         width: '640',
-        videoId: 'M7lc1UVf-VE',
+        videoId: 'vUySIPTxtvQ',
         events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange,
